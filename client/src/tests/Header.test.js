261aa5ccdc29480e6debf6cb085fb7def117a935
vjz3qz <vjz3qz@virginia.edu>
2024-03-23 14:24:07 -0400

    add client tests

---
+++ client/src/tests/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen, cleanup } from "@testing-library/react";
+// import Header from "../../../client/src/components/Header";
+import "@testing-library/jest-dom";
+
+// test("renders Header component", () => {
+//   render(<Header />);
+//   const mainElement = screen.getByTestId("header-banner");
+//   expect(mainElement).toBeInTheDocument();
+//   cleanup();
+// });
+
+test("test", () => {
+  expect(1).toBe(1);
+});
