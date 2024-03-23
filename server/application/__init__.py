25f0637e9d4c074adfe83df49f735eddf26651a1
vjz3qz <vjz3qz@virginia.edu>
2024-03-23 13:50:52 -0400

    Add vector_db and nlp_service imports

--- server/application/__init__.py
+++ server/application/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from application.api.v1 import v1 as v1_blueprint
 import logging
-from application.services.database_service import init_db
+from application.services.database_service import vector_db
+from application.services.nlp_service import (embeddings, chat_model, llm)
 from dotenv import load_dotenv
 from flask import send_from_directory
 import os
