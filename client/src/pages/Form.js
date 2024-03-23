1e3b0be2ecddc6d640a9e3e510f8361ca34e1735
niketanand-7 <niketanand7@gmail.com>
2024-03-23 13:54:16 -0400

    updated form

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, TextField, Button, Select, MenuItem, InputLabel, FormControl, Typography } from '@mui/material';
+import { Container, TextField, Button, Select, MenuItem, InputLabel, FormControl, Typography, Box } from '@mui/material';
 
 function LanguageLearningForm() {
   const [form, setForm] = useState({
@@ -30,69 +30,80 @@ function LanguageLearningForm() {
 
   return (
     <Container maxWidth="sm">
-      <Typography variant="h4" component="h1" gutterBottom>
-        Language Learning Profile
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        {/* Name Field */}
-        <TextField
-          fullWidth
-          label="What should we call you?"
-          name="name"
-          value={form.name}
-          onChange={handleChange}
-          margin="normal"
-        />
-        
-        {/* Language Selector */}
-        <FormControl fullWidth margin="normal">
-          <InputLabel id="language-selector-label">Select the language you are trying to learn</InputLabel>
-          <Select
-            labelId="language-selector-label"
-            name="language"
-            value={form.language}
+        <Box sx={{
+        bgcolor: 'background.paper', // or any light shade color
+        p: 4,
+        borderRadius: 2,
+        boxShadow: 1,
+        mt: 2,
+        mb: 3,
+      }}>
+            <Typography variant="h4" component="h1" gutterBottom>
+            Language Learning Profile
+        </Typography>
+        <form onSubmit={handleSubmit}>
+            {/* Name Field */}
+            <TextField
+            fullWidth
+            label="What should we call you?"
+            name="name"
+            value={form.name}
             onChange={handleChange}
-          >
-            <MenuItem value="Spanish">Spanish</MenuItem>
-            {/* Add more languages here */}
-          </Select>
-        </FormControl>
-        
-        {/* Reason for Learning */}
-        <TextField
-          fullWidth
-          label="Why are you looking to practice this language?"
-          name="reason"
-          value={form.reason}
-          onChange={handleChange}
-          margin="normal"
-        />
-        
-        {/* Previous Language Knowledge */}
-        <TextField
-          fullWidth
-          label="Specify any previous language knowledge."
-          name="previousKnowledge"
-          value={form.previousKnowledge}
-          onChange={handleChange}
-          margin="normal"
-        />
-        
-        {/* Interests */}
-        <TextField
-          fullWidth
-          label="List any of your interests."
-          name="interests"
-          value={form.interests}
-          onChange={handleChange}
-          margin="normal"
-        />
-        
-        {/* Submit Button */}
-        <Button variant="contained" color="primary" type="submit" style={{ marginTop: '20px' }}>
-          Submit Profile
-        </Button>
-      </form>
+            margin="normal"
+            />
+            
+            {/* Language Selector */}
+            <FormControl fullWidth margin="normal" variant="outlined" sx={{ marginBottom: 2 }}>
+                <InputLabel id="language-selector-label">Select the language you are trying to learn</InputLabel>
+                <Select
+                labelId="language-selector-label"
+                name="language"
+                value={form.language}
+                onChange={handleChange}
+                label="Select the language you are trying to learn"
+                sx={{ backgroundColor: '#fff' }}
+                >
+                <MenuItem value="Spanish">Spanish</MenuItem>
+                {/* ... add more languages ... */}
+                </Select>
+            </FormControl>
+            
+            {/* Reason for Learning */}
+            <TextField
+            fullWidth
+            label="Why are you looking to practice this language?"
+            name="reason"
+            value={form.reason}
+            onChange={handleChange}
+            margin="normal"
+            />
+            
+            {/* Previous Language Knowledge */}
+            <TextField
+            fullWidth
+            label="Specify any previous language knowledge."
+            name="previousKnowledge"
+            value={form.previousKnowledge}
+            onChange={handleChange}
+            margin="normal"
+            />
+            
+            {/* Interests */}
+            <TextField
+            fullWidth
+            label="List any of your interests."
+            name="interests"
+            value={form.interests}
+            onChange={handleChange}
+            margin="normal"
+            />
+            
+            {/* Submit Button */}
+            <Button variant="contained" color="primary" type="submit" style={{ marginTop: '20px' }}>
+            Submit Profile
+            </Button>
+        </form>
+      </Box>
     </Container>
   );
 }
