3b34bbdaea4ea0f3a65b700c2276a68897ff62be
niketanand-7 <niketanand7@gmail.com>
2024-03-23 13:39:04 -0400

    routes updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import HomePage from './HomePage';
 import ChatWrapper from "./pages/ChatWrapper";
-import Form from "./pages/Form";
+import Form from './pages/Form';
 
 function App() {
   return (
-    <div className="content-wrapper">
-      <Form />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        {/* <Route path="/home" element={<HomePage />} /> */}
+        <Route path="/chat" element = {<ChatWrapper />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
