7aeb2b9a30fae8517b8322a60779885a3c3fdcdb
arjuntrivedi <arjuntrivedi11@gmail.com>
2024-03-23 14:40:54 -0400

    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ChatWrapper from "./pages/ChatWrapper";
 import Form from './pages/Form';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Form />} />
+        <Route path="/" element={<Login />} />
         <Route path="/form" element={<Form />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/chat" element = {<ChatWrapper />} />
