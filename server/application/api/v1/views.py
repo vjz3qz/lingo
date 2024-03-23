414c0a82ac29a2001172aad9bc4986a4495c27ed
vjz3qz <vjz3qz@virginia.edu>
2024-03-23 13:45:00 -0400

    Update .gitignore and comment out unused imports in views.py

--- server/application/api/v1/views.py
+++ server/application/api/v1/views.py
@@ -1,14 +1,14 @@
 from . import v1
 
 import logging
-from application.controllers.chat_controller import (
-    get_chat_response,
-)
+# from application.controllers.chat_controller import (
+#     get_chat_response,
+# )
 
-from application.services.database_service import (
-    get_chat_history,
-    save_chat_history,
-)
+# from application.services.database_service import (
+#     get_chat_history,
+#     save_chat_history,
+# )
 
 
 from flask import jsonify, request
