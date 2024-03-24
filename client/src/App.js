5ccd347205b5431e7cb31a86ab491872126f71bc
vjz3qz <vjz3qz@virginia.edu>
2024-03-23 21:18:34 -0400

    Refactor routes and add AuthenticationProvider context

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 import ChatWrapper from "./pages/ChatWrapper";
-import Form from './pages/Form';
-import Login from './pages/Login';
-import Proficiency from './pages/Proficiency';
-
+import Form from "./pages/Form";
+import Proficiency from "./pages/Proficiency";
+import Authentication from "./pages/Authentication";
+import { AuthProvider } from "./contexts/AuthenticationProvider";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/auth" element={<Authentication />} />
         <Route path="/form" element={<Form />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/chat" element = {<ChatWrapper />} />
-        <Route path="/proficiency" element = {<Proficiency />} />
+        <Route path="/chat" element={<ChatWrapper />} />
+        <Route path="/proficiency" element={<Proficiency />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
---
+++ client/src/pages/Authentication.js
@@ -0,0 +1,185 @@
+// import * as React from "react";
+// import Button from "@mui/material/Button";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import TextField from "@mui/material/TextField";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Checkbox from "@mui/material/Checkbox";
+// import Link from "@mui/material/Link";
+// import Grid from "@mui/material/Grid";
+// import Box from "@mui/material/Box";
+// // import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from "@mui/material/Typography";
+// import Container from "@mui/material/Container";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+
+// function Copyright(props) {
+//   return (
+//     <Typography
+//       variant="body2"
+//       color="text.secondary"
+//       align="center"
+//       {...props}
+//     >
+//       {"Copyright © "}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{" "}
+//       {new Date().getFullYear()}
+//       {"."}
+//     </Typography>
+//   );
+// }
+
+// // TODO remove, this demo shouldn't need to reset the theme.
+
+// const defaultTheme = createTheme();
+
+// export default function SignIn() {
+//   const navigate = useNavigate();
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get("email"),
+//       password: data.get("password"),
+//     });
+//     try {
+//       const response = await axios.post("/api/v1/log-in", {
+//         email: data.get("email"),
+//         password: data.get("password"),
+//       });
+//       console.log(response);
+//       if (response.status === 200) {
+//         navigate("/home");
+//       } else {
+//         console.log("Login failed");
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <ThemeProvider theme={defaultTheme}>
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <Box
+//           sx={{
+//             marginTop: 8,
+//             display: "flex",
+//             flexDirection: "column",
+//             alignItems: "center",
+//           }}
+//         >
+//           {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+//             <LockOutlinedIcon />
+//           </Avatar> */}
+//           <Typography component="h1" variant="h5">
+//             Sign in
+//           </Typography>
+//           <Box
+//             component="form"
+//             onSubmit={handleSubmit}
+//             noValidate
+//             sx={{ mt: 1 }}
+//           >
+//             <TextField
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               autoComplete="email"
+//               autoFocus
+//             />
+//             <TextField
+//               margin="normal"
+//               required
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               id="password"
+//               autoComplete="current-password"
+//             />
+//             <FormControlLabel
+//               control={<Checkbox value="remember" color="primary" />}
+//               label="Remember me"
+//             />
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               sx={{ mt: 3, mb: 2 }}
+//             >
+//               Sign In
+//             </Button>
+//             <Grid container>
+//               <Grid item xs>
+//                 <Link href="#" variant="body2">
+//                   Forgot password?
+//                 </Link>
+//               </Grid>
+//               <Grid item>
+//                 <Link href="#" variant="body2">
+//                   {"Don't have an account? Sign Up"}
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </Box>
+//         </Box>
+//         <Copyright sx={{ mt: 8, mb: 4 }} />
+//       </Container>
+//     </ThemeProvider>
+//   );
+// }
+
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
+const supabase = createClient(
+  "https://ggqxyucbtqxzubhqavlk.supabase.co", // Use the environment variable for the URL
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdncXh5dWNidHF4enViaHFhdmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMTU1MjAsImV4cCI6MjAyNjc5MTUyMH0.UFUd28pIaBKzn-f0JH9fuoU0FkqifM7nDyQ9bWKtFOo" // Use the environment variable for the key
+);
+
+export default function Authentication() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <div
+        style={{
+          width: "60%",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "10px",
+        }}
+      >
+        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
+    );
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
