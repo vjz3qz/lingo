e59d88012c9a6613448714978aa0e8a150d867a8
vjz3qz <vjz3qz@virginia.edu>
2024-03-24 22:00:22 -0400

    Refactor chat_controller.py to use system messages and placeholders

--- server/application/controllers/chat_controller.py
+++ server/application/controllers/chat_controller.py
@@ -1,52 +1,48 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, MessagesPlaceholder
 from application.services.database_service import db_client, get_user_data, get_user_last_proficiency_by_language, create_proficiency_record
 from application.services.nlp_service import chat_model
 from application.utils.parse_conversation import parse_conversation
 import logging
-# from application.services.transcription_service import transcribe_audio
 
 
 
 
 def get_chat_response(conversation_history, user_id, language):
     conversation_history = parse_conversation(conversation_history)
-    name, interests = get_user_data(user_id)
+    name, previous_knowledge, interests = get_user_data(user_id)
     previous_proficiency_score, previous_proficiency_feedback = get_user_last_proficiency_by_language(user_id, language)
+
     template = """You are a language expert in {language} who is doing having a conversation with, {name}. 
-                    This is their interests: {interests}
-                    This is their previous proficiency feedback in {language}: {previous_proficiency_feedback}
-                    Here is your conversation history: 
+                        This is their interests: {interests}
+                        This is their previous proficiency feedback in {language}: {previous_proficiency_feedback}
+                        """
 
-                    {conversation_history}
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    prompt = ChatPromptTemplate.from_messages([system_message_prompt, MessagesPlaceholder(variable_name="messages")])
 
-                    If there is no conversation history, start a conversation with {name} about their interests.
-                    If there is conversation history, continue the conversation with {name}.
-                    Your response:
-                    """
-    
-    chat_prompt = ChatPromptTemplate.from_template(template)
-    response = chat(chat_prompt.format_messages(
-    language=language,
-    name=name,
-    interests=interests,
-    previous_proficiency_feedback=previous_proficiency_feedback,
-    conversation_history=conversation_history
-    ))
-    return response
+    chain = prompt | chat_model
+
+    response = chain.invoke(
+        {
+            "language": language,
+            "name": name,
+            "interests": interests,
+            "previous_proficiency_feedback": previous_proficiency_feedback,
+            "messages": conversation_history,
+        }
+    )
+    response_text = response.content
+    return response_text
+
 
 
 def analyze_proficiency(conversation_history, user_id, language):
     conversation_history = parse_conversation(conversation_history)
-    name, interests = get_user_data(user_id)
+    name, previous_knowledge, interests = get_user_data(user_id)
     previous_proficiency_score, previous_proficiency_feedback = get_user_last_proficiency_by_language(user_id, language)
     template = """You are a language expert in {language} who finished having a conversation with, {name}.
                     This is their previous proficiency level in {language}: {previous_proficiency_score}
                     This is their previous proficiency feedback in {language}: {previous_proficiency_feedback}
-                    Here is your conversation history: 
-
-                    {conversation_history}
-
                     Analyze {name}'s proficiency in {language} based on the conversation history, providing 
                     insightful and constructive feedback and an updated proficiency score. The updated proficiency 
                     score should be an integer. If there was no previous proficiency score, provide a proficiency 
@@ -56,20 +52,26 @@ def analyze_proficiency(conversation_history, user_id, language):
                     Proficiency Score: [score]
                     Feedback: [feedback]
                     """
-    
-    chat_prompt = ChatPromptTemplate.from_template(template)
-    response = chat(chat_prompt.format_messages(
-    language=language,
-    name=name,
-    interests=interests,
-    previous_proficiency_feedback=previous_proficiency_feedback,
-    previous_proficiency_score=previous_proficiency_score,
-    conversation_history=conversation_history
-    ))
 
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    prompt = ChatPromptTemplate.from_messages([system_message_prompt, MessagesPlaceholder(variable_name="messages")])
+
+    chain = prompt | chat_model
+
+    response = chain.invoke(
+        {
+            "language": language,
+            "name": name,
+            "interests": interests,
+            "previous_proficiency_feedback": previous_proficiency_feedback,
+            "messages": conversation_history,
+        }
+    )
+
+    response_text = response.content
 
     # Parse the response to get the proficiency score and feedback
-    lines = response.split('\n')
+    lines = response_text.split('\n')
     proficiency_score = int(lines[0].split(': ')[1])
     feedback = lines[1].split(': ')[1]
 
